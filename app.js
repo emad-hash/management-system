32beab641deaf59191700d858ff336352cc0cd35
emad-hash <emadhashash76@gmail.com>
2022-08-17 03:07:54 +0300

    emad

--- app.js
+++ app.js
@@ -65,33 +65,69 @@ th4.textContent = "salary";
 thead.append(th4);
 table.prepend(thead);
 
-const em1 =   new Employee(1000, "Ghazi Samer", "Administration", "Senior");
-render(em1.id,em1.fullName,em1.dep,em1.level,em1.salary);
+// const em1 =   new Employee(1000, "Ghazi Samer", "Administration", "Senior");
+// render(em1.id,em1.fullName,em1.dep,em1.level,em1.salary);
 
-const em2 = new Employee(1001, "Lana Ali", "Finance", "Senior");
-render(em2.id,em2.fullName,em2.dep,em2.level,em2.salary);
+// const em2 = new Employee(1001, "Lana Ali", "Finance", "Senior");
+// render(em2.id,em2.fullName,em2.dep,em2.level,em2.salary);
 
-const em3 =new Employee(1002, "Tamara Ayoub ", "Marketing", "Senior");
-render(em3.id,em3.fullName,em3.dep,em3.level,em3.salary);
+// const em3 =new Employee(1002, "Tamara Ayoub ", "Marketing", "Senior");
+// render(em3.id,em3.fullName,em3.dep,em3.level,em3.salary);
 
-const em4 =new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
-render(em4.id,em4.fullName,em4.dep,em4.level,em4.salary);
+// const em4 =new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
+// render(em4.id,em4.fullName,em4.dep,em4.level,em4.salary);
 
-const em5 =new Employee(1004, "Omar Zaid", "Development", "Senior");
-render(em5.id,em5.fullName,em5.dep,em5.level,em5.salary);
+// const em5 =new Employee(1004, "Omar Zaid", "Development", "Senior");
+// render(em5.id,em5.fullName,em5.dep,em5.level,em5.salary);
 
-const em6 = new Employee(1005, "Rana Saleh", "Development", "Junior");
-render(em6.id,em6.fullName,em6.dep,em6.level,em6.salary);
+// const em6 = new Employee(1005, "Rana Saleh", "Development", "Junior");
+// render(em6.id,em6.fullName,em6.dep,em6.level,em6.salary);
 
-const em7 =  new Employee(1006, "Ghazi Samer", "Finance", "Mid-Senior");
-render(em7.id,em7.fullName,em7.dep,em7.level,em7.salary);
+// const em7 =  new Employee(1006, "Ghazi Samer", "Finance", "Mid-Senior");
+// render(em7.id,em7.fullName,em7.dep,em7.level,em7.salary);
 
 function render(...rows) {
     let tr = document.createElement("tr");
+    let persons = [];
     for (const row of rows){
         const td = document.createElement("td");
         td.textContent = row ;
         tr.append(td);
+        persons.push(row)
+
     }
     table.append(tr);
+    setLocalStorage(persons);
 }
+
+
+// events 
+let submit = document.getElementById("submit");
+let id = document.getElementById("number")
+let fullname = document.getElementById("fullname")
+let imgUrl = document.getElementById("img")
+let department = document.getElementById("Departments")
+let level = document.getElementById("level")
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    
+    const emp = new Employee(number.value,fullname.value,Department.value,level.value,imgUrl.value);
+    render(number.value,fullname.value,Department.value,level.value,emp.salary);
+    number.value = "";
+    fullname.value = "";
+    department.value = department[0].value ;
+    level.value = level[0].value;
+    imgUrl.value = "";
+    })
+
+let count = 0;
+function setLocalStorage (...persons){
+    for (let i = 0; i < persons.length; i++) {
+        localStorage.setItem(`persons ${count}`,JSON.stringify(persons))
+        count ++;
+    }
+}
+
+// event card 
+
